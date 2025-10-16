2e6fa3092b56d4210068311f9ebb641d865be630
abportillo02 <abportillo@coh.org>
2025-10-16 11:14:50 -0700

    .sh

---
+++ scripts/fastq_dump.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=sra_download
+#SBATCH --output=sra_download.out
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=abportillo@coh.org
+#SBATCH -n 16 # Number of cores
+#SBATCH -N 1-4 # Min - Max Nodes
+#SBATCH -p all
+#SBATCH --mem=150G
+#SBATCH --time=48:00:00
+
+# Load Conda environment
+source source /home/abportillo/.bashrc
+conda activate mamba_abner_BC
+
+# Output directory
+output_dir="/home/abportillo/github_repo/Single_cell/fastq"
+mkdir -p "$output_dir"
+
+# SRA IDs
+sra_ids = "/home/abportillo/github_repo/Single_cell/SRR_Acc_List.txt"
+
+# Loop through and download each
+for sra in "${sra_ids}"; do
+  echo "Downloading $sra..."
+  fasterq-dump "$sra" -O "$output_dir" -e 4
+done
+
+echo "All downloads complete."
